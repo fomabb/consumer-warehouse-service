95e0de9c6e404345ba52b6bb7978f04bd9183fa6
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-05-26 17:01:25 +0300

    finish

--- src/main/java/com/example/warehouse_service/kafka/OrderKafkaConsumer.java
+++ src/main/java/com/example/warehouse_service/kafka/OrderKafkaConsumer.java
@@ -1,6 +1,7 @@
 package com.example.warehouse_service.kafka;
 
 import com.example.warehouse_service.dto.Order;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,7 +13,11 @@ public class OrderKafkaConsumer {
     private static final Logger log = LoggerFactory.getLogger(OrderKafkaConsumer.class);
 
     @KafkaListener(topics = "orders", groupId = "warehouse-group")
-    public void consumerOrder(Order order) {
-        log.info("Received order: order={}", order);
+    public void consumerOrder(ConsumerRecord<String, Order> record) {
+        log.info("Received order: order={}, key={}, partition={}",
+                record.value(),
+                record.key(),
+                record.partition()
+        );
     }
 }
